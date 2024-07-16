43efee3d8c6d0bc04613ea49eb37e11ad452e658
Work <yakushevso@ya.ru>
2024-07-16 09:03:34 +0300

    Stage 5/6:Define rules to end the game

--- Battleship with Java/task/src/battleship/Game.java
+++ Battleship with Java/task/src/battleship/Game.java
@@ -16,14 +16,25 @@ public class Game {
 
         System.out.println(Messages.TAKE_SHOT);
 
-        switch (field.takeShot()) {
-            case HIT -> {
-                field.printField(field.getFogField());
-                System.out.println(Messages.HIT);
-            }
-            case MISS -> {
-                field.printField(field.getFogField());
-                System.out.println(Messages.MISS);
+        int count = 0;
+        while (count < 5) {
+            switch (field.takeShot()) {
+                case HIT -> {
+                    field.printField(field.getFogField());
+                    System.out.println(Messages.HIT);
+                }
+                case MISS -> {
+                    field.printField(field.getFogField());
+                    System.out.println(Messages.MISS);
+                }
+                case SANK -> {
+                    field.printField(field.getFogField());
+                    if (++count == 5) {
+                        System.out.println(Messages.WON);
+                    } else {
+                        System.out.println(Messages.SANK);
+                    }
+                }
             }
         }
 
--- Battleship with Java/task/src/battleship/Messages.java
+++ Battleship with Java/task/src/battleship/Messages.java
@@ -8,10 +8,11 @@ public enum Messages {
     ERROR_CLOSE("Error! You placed it too close to another one. Try again:\n"),
     GAME_STARTS("The game starts!"),
     TAKE_SHOT("Take a shot!"),
-    HIT("You hit a ship!"),
-    MISS("You missed!"),
+    HIT("You hit a ship! Try again:"),
+    MISS("You missed. Try again:"),
     WON("You sank the last ship. You won. Congratulations!"),
     SANK("You sank a ship! Specify a new target:");
+
 
     private final String message;
 
